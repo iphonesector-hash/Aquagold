d5bae56511518e4c182266e310399899dd5a418f
iphonesector-hash <iphone.sector@gmail.com>
2026-09-02 18:31:10 +0330

    Switch Round6 injection to Alpine-safe UI

--- aqua_round4_ui_fix.py
+++ aqua_round4_ui_fix.py
@@ -14,9 +14,9 @@ def aqua_round5_user_fixes_js():
     return app_v3.send_from_directory(".", "aqua-round5-user-fixes.js", mimetype="application/javascript", max_age=0)
 
 
-@app_v3.app.get("/aqua-round6-user-fixes.js")
-def aqua_round6_user_fixes_js():
-    return app_v3.send_from_directory(".", "aqua-round6-user-fixes.js", mimetype="application/javascript", max_age=0)
+@app_v3.app.get("/aqua-round6-safe-ui.js")
+def aqua_round6_safe_ui_js():
+    return app_v3.send_from_directory(".", "aqua-round6-safe-ui.js", mimetype="application/javascript", max_age=0)
 
 
 @app_v3.app.after_request
@@ -38,10 +38,10 @@ def inject_aqua_round4_ui(response):
                 '<script src="/aqua-round5-user-fixes.js?v=20260902-1"></script></body>',
                 1,
             )
-        if '/aqua-round6-user-fixes.js?' not in body:
+        if '/aqua-round6-safe-ui.js?' not in body:
             body = body.replace(
                 "</body>",
-                '<script src="/aqua-round6-user-fixes.js?v=20260902-1"></script></body>',
+                '<script src="/aqua-round6-safe-ui.js?v=20260902-2"></script></body>',
                 1,
             )
         response.set_data(body)
