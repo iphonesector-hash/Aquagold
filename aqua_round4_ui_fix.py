8dd313c4c024f4314e88512016033d73807ec29b
iphonesector-hash <iphone.sector@gmail.com>
2026-09-02 18:02:51 +0330

    Inject Round6 Aqua UI after prior scoped layers

--- aqua_round4_ui_fix.py
+++ aqua_round4_ui_fix.py
@@ -14,6 +14,11 @@ def aqua_round5_user_fixes_js():
     return app_v3.send_from_directory(".", "aqua-round5-user-fixes.js", mimetype="application/javascript", max_age=0)
 
 
+@app_v3.app.get("/aqua-round6-user-fixes.js")
+def aqua_round6_user_fixes_js():
+    return app_v3.send_from_directory(".", "aqua-round6-user-fixes.js", mimetype="application/javascript", max_age=0)
+
+
 @app_v3.app.after_request
 def inject_aqua_round4_ui(response):
     try:
@@ -31,6 +36,12 @@ def inject_aqua_round4_ui(response):
             body = body.replace(
                 "</body>",
                 '<script src="/aqua-round5-user-fixes.js?v=20260902-1"></script></body>',
+                1,
+            )
+        if '/aqua-round6-user-fixes.js?' not in body:
+            body = body.replace(
+                "</body>",
+                '<script src="/aqua-round6-user-fixes.js?v=20260902-1"></script></body>',
                 1,
             )
         response.set_data(body)
